7c958cb46812484849f39c942138eea8f699b8a8
iegorch86 <66792505+iegorch86@users.noreply.github.com>
2026-09-16 21:25:35 -0400

    Register WebUI Users administration routes

--- internal/server/server.go
+++ internal/server/server.go
@@ -96,6 +96,7 @@ func (a *App) Handler() http.Handler {
 	mux.HandleFunc("POST /password", a.providerPasswordChange)
 	mux.HandleFunc("GET /settings/authentication", a.authenticationPage)
 	mux.HandleFunc("POST /settings/authentication", a.authenticationChange)
+	a.registerAdminUsersRoutes(mux)
 	mux.HandleFunc("GET /api/dashboard-status", a.dashboardStatus)
 	mux.HandleFunc("POST /minecraft/start", a.minecraftAction("start"))
 	mux.HandleFunc("POST /minecraft/stop", a.minecraftAction("stop"))
